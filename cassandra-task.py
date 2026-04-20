526e400a56841619c52f0f9cea35fd5f38d4b27c
Shereen Mohamed <163689317+Shereen-Mohamed36@users.noreply.github.com>
2026-04-21 01:07:37 +0200

    Add additional student records

--- cassandra-task.py
+++ cassandra-task.py
@@ -1,12 +1,12 @@
 from cassandra.cluster import Cluster
 
-# 🔹 Connect to Cassandra
+#  Connect to Cassandra
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 
 print("Connected to Cassandra")
 
-# 🔹 Create Keyspace
+#  Create Keyspace
 session.execute("""
 CREATE KEYSPACE IF NOT EXISTS university
 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
@@ -14,7 +14,7 @@ WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
 
 session.set_keyspace('university')
 
-# 🔹 Create Table (Composite Primary Key)
+#  Create Table (Composite Primary Key)
 session.execute("""
 CREATE TABLE IF NOT EXISTS students (
     department TEXT,
@@ -28,16 +28,19 @@ CREATE TABLE IF NOT EXISTS students (
 
 print("Table created")
 
-# 🔹 Insert Data (5 rows)
+#  Insert Data (5 rows)
 session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('CS', 1, 'Ali', 21, 3.2)")
 session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('CS', 2, 'Sara', 22, 3.8)")
 session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('IT', 1, 'Mona', 20, 3.5)")
 session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('IT', 2, 'Omar', 23, 2.9)")
 session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('AI', 1, 'Youssef', 21, 3.7)")
+session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('IS', 1, 'Ahmed', 21, 3.8)")
+session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('IS', 2, 'Ola', 20, 3.3)")
+session.execute("INSERT INTO students (department, student_id, name, age, grade) VALUES ('IS', 3, 'Eman', 21, 2.5)")
 
 print("Data inserted")
 
-# 🔹 Update a value
+#  Update a value
 session.execute("""
 UPDATE students SET grade = 4.0
 WHERE department = 'CS' AND student_id = 1
@@ -45,7 +48,7 @@ WHERE department = 'CS' AND student_id = 1
 
 print("Data updated")
 
-# 🔹 Delete a row
+#  Delete a row
 session.execute("""
 DELETE FROM students
 WHERE department = 'IT' AND student_id = 2
@@ -53,7 +56,7 @@ WHERE department = 'IT' AND student_id = 2
 
 print("Row deleted")
 
-# 🔹 Show data
+#  Show data
 rows = session.execute("SELECT * FROM students")
 for row in rows:
     print(row)
